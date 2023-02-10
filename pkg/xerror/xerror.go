3bc0547dd0787ff30233745e1ee99a4b5fb2566b
Sina Safari <sina.devel@gmail.com>
2023-02-10 13:30:21 +0330

    feat: add more functionality to xerror.

--- pkg/xerror/xerror.go
+++ pkg/xerror/xerror.go
@@ -1,7 +1,78 @@
 package xerror
 
+import (
+	"errors"
+	"fmt"
+	"runtime"
+)
+
 func PanicIf(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Add adds context to the error.
+// The result cannot be unwrapped to recover the original error.
+// It does nothing when *errp == nil.
+//
+// Example:
+//
+//	defer xerror.Add(&err, "copy(%s, %s)", src, dst)
+//
+// See Wrap for an equivalent function that allows
+// the result to be unwrapped.
+func Add(errp *error, format string, args ...any) {
+	if *errp != nil {
+		*errp = fmt.Errorf("%s: %v", fmt.Sprintf(format, args...), *errp)
+	}
+}
+
+// Wrap adds context to the error and allows
+// unwrapping the result to recover the original error.
+//
+// Example:
+//
+//	defer xerror.Wrap(&err, "copy(%s, %s)", src, dst)
+//
+// See Add for an equivalent function that does not allow
+// the result to be unwrapped.
+func Wrap(errp *error, format string, args ...any) {
+	if *errp != nil {
+		*errp = fmt.Errorf("%s: %w", fmt.Sprintf(format, args...), *errp)
+	}
+}
+
+// WrapStack is like Wrap, but adds a stack trace if there isn't one already.
+func WrapStack(errp *error, format string, args ...any) {
+	if *errp != nil {
+		if se := (*StackError)(nil); !errors.As(*errp, &se) {
+			*errp = NewStackError(*errp)
+		}
+		Wrap(errp, format, args...)
+	}
+}
+
+// StackError wraps an error and adds a stack trace.
+type StackError struct {
+	Stack []byte
+	err   error
+}
+
+// NewStackError returns a StackError, capturing a stack trace.
+func NewStackError(err error) *StackError {
+	var buf [16 * 1024]byte
+	n := runtime.Stack(buf[:], false)
+	return &StackError{
+		err:   err,
+		Stack: buf[:n],
+	}
+}
+
+func (e *StackError) Error() string {
+	return e.err.Error() // ignore the stack
+}
+
+func (e *StackError) Unwrap() error {
+	return e.err
 }
